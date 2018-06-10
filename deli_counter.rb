bf9f0af7c811e39a2f5247228f819c5773a9c963
Sean Conrad <seanconrad123@gmail.com>
2018-06-10 02:20:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 
 def line(line_list)
   if line_list.length < 1
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     str = "The line is currently: "
     line_list.each_with_index{|name, number| str += "#{number}. #{name} "}
