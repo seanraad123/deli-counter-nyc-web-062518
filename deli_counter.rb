1f38850c22d169b767c8b47e672aaf3250d23aca
Sean Conrad <seanconrad123@gmail.com>
2018-06-10 01:56:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ line = []
 
 def line(line_list)
   if line_list.length < 1
-    print "The line is currently empty."
+    puts "The line is currently empty."
   else
-    print line_list
+    puts line_list
   end
 end
