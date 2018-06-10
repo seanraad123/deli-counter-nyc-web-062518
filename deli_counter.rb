d750f119b6b9d801176f1d714443547dad1b61d2
Sean Conrad <seanconrad123@gmail.com>
2018-06-10 02:16:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,6 @@ def line(line_list)
   else
     str = "The line is currently: "
     for i in line_list
-      puts "a"
       line_list.each_with_index{|name, number| str += "#{number}. #{name}"}
     end
   end
