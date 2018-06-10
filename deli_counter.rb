29838d1a6377617816efefbc75dde10591ed129f
Sean Conrad <seanconrad123@gmail.com>
2018-06-10 01:56:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 line = []
 
-def line
-  if line_list.length < 1
+def line(line)
+  if line.length < 1
     print "The line is currently empty."
   else
-    print line_list
+    print line
   end
 end
